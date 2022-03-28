66c94a8acdae8d3c13949c99968dfa39b893b1e3
dustin-ward <wardddustin@gmail.com>
2022-03-28 13:13:17 -0600

    Add gracefulStop

--- server.go
+++ server.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"testing/grpc-demo/chat"
 
 	"google.golang.org/grpc"
 )
+
+func shutdown(s *grpc.Server) {
+	var input string
+	fmt.Scanln(&input)
+	for !(input == "stop" || input == "STOP") {
+		fmt.Scanln(&input)
+	}
+	s.GracefulStop()
+	os.Exit(0)
+}
 
 func main() {
 	lis, err := net.Listen("tcp", ":9000")
@@ -19,6 +31,7 @@ func main() {
 	s := chat.Server{}
 	chat.RegisterChatServiceServer(grpcServer, &s)
 
+	go shutdown(grpcServer)
 	log.Println("Listening on :9000...")
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Serve: %v", err)
